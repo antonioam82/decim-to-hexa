3d90ab155bbe73425c2cd901b99067af9f1e253b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-02-20 21:56:01 +0100

    Update hexaded_num.py

--- hexaded_num.py
+++ hexaded_num.py
@@ -2,23 +2,24 @@ from VALID import OKI, ns
 import subprocess
 #CONVERTIR DECIMAL EN HEXADECIMAL
 
-while True:
-    def DecimalAHexadecimal():
+def DecimalAHexadecimal():
         decimal = OKI(input("Introduzca número: "))
         hexadecimal = ""
         while decimal != 0: 
             rem = CambiarDigitos(decimal % 16)
             hexadecimal = str(rem) + hexadecimal
             decimal = int(decimal / 16)
-        print("Resultado: " + hexadecimal) 
+        print("Resultado: " + hexadecimal)
 
-    def CambiarDigitos(digitos):
+def CambiarDigitos(digitos):
         decimales =     [10 , 11 , 12 , 13 , 14 , 15 ]
         hexadecimal = ["A", "B", "C", "D", "E", "F"]
         for c in range(7):
             if digitos == decimales[c - 1]:
                 digitos = hexadecimal[c - 1]
         return digitos
+
+while True:
 
     DecimalAHexadecimal()
     conti=ns(input("¿Desea continuar?: "))
